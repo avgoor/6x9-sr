430b225934d0eae1789aef6c326e9956452aa3b5
Denis V. Meltsaykin <dvm@avgoor.ru>
2019-03-30 16:05:02 +0100

    Reworked "frame-buffer"
    Now the matrix is stored as byte[9] which represents each column
    as a byte (6 most significant bits are rows, 2 other bits do not
    matter). Reworked also shifting and random generation to be way
    more effective.

--- 6x9-sr.ino
+++ 6x9-sr.ino
@@ -2,75 +2,69 @@
 #define NCOLS 9
 
 #define LATCH 2
-#define SER   3
-#define CLK   4
+#define SER 3
+#define CLK 4
 
+/* the matrix is a "frame-buffer",
+ * each byte here represents a column
+ * but since the display has only 6 rows
+ * 2 less significant bits are useless
+ */
+byte matrix[NCOLS] = {
+    0b11111100,
+    0b10100100,
+    0b10100100,
+    0b10011100,
+    0b10000100,
+    0b11100100,
+    0b10100100,
+    0b10100100,
+    0b11111100};
 
-char matrix[NROWS][NCOLS] = {
-  {1, 1, 1, 1, 1, 1, 1, 1, 1},
-  {1, 0, 0, 0, 0, 1, 0, 0, 1},
-  {1, 1, 1, 0, 0, 1, 1, 1, 1},
-  {1, 0, 0, 1, 0, 0, 0, 0, 1},
-  {1, 0, 0, 1, 0, 0, 0, 0, 1},
-  {1, 1, 1, 1, 1, 1, 1, 1, 1}
-};
-
-
-void setup() {
-  pinMode (LATCH, OUTPUT);
-  pinMode (SER, OUTPUT);
-  pinMode (CLK, OUTPUT);
-  //  Serial.begin(9600);
+void setup()
+{
+  pinMode(LATCH, OUTPUT);
+  pinMode(SER, OUTPUT);
+  pinMode(CLK, OUTPUT);
+  //Serial.begin(9600);
 }
 
-void push_rnd() {
-  char tmp;
-  for (int i = NROWS - 1; i > 0; i--) {
-    for (int j = 0; j < NCOLS; j++) {
-      tmp = matrix[i - 1][j];
-      matrix[i][j] = tmp;
-    };
+void push_rnd(){
+  unsigned long int randomNumber = random(0,65535);
+  for (int i = 0; i < NCOLS; i++){
+    matrix[i] = matrix[i] >> 1; 
+    matrix[i] |= (randomNumber >> i) << 7;
   };
-  matrix[0][0] = random(0, 2);
-  matrix[0][1] = random(0, 2);
-  matrix[0][2] = random(0, 2);
-  matrix[0][3] = random(0, 2);
-  matrix[0][4] = random(0, 2);
-  matrix[0][5] = random(0, 2);
-  matrix[0][6] = random(0, 2);
-  matrix[0][7] = random(0, 2);
-  matrix[0][8] = random(0, 2);
 };
 
-
-void draw_cycle() {
-  // drawing by columns
-  unsigned int data = 0b1111111111000000; // first 6 bits are rows addresses all others are columns
-  for (int y = 0; y < NCOLS; y++) {
-    data = 0b1111111111000000;
+void draw_cycle()
+{
+  /* drawing by columns
+   * 6 less significant bits are rows addresses
+   * all others are columns */
+  unsigned int data = 0b0000001111111111;
+  for (int y = 0; y < NCOLS; y++){
+    data = 0b0000001111111111;
     digitalWrite(LATCH, LOW);
-    data |= matrix[0][y] << 0;
-    data |= matrix[1][y] << 1;
-    data |= matrix[2][y] << 2;
-    data |= matrix[3][y] << 3;
-    data |= matrix[4][y] << 4;
-    data |= matrix[5][y] << 5;
-    data ^= (1 << (14 - y)); // activating y-th column
-    /*    char strx[64];
+    data |= (matrix[y] & 0xFC) << 8;
+    data ^= (1 << (1 + y)); // activating y-th column; it's reversed since screen is reversed
+/*
+     char strx[64];
         sprintf(strx, "%08X:00ff=(%02X):ff00=(%02X)\n", data, data & 0x00FF, data & 0xFF00);
         Serial.write(strx);
-    */
-    shiftOut(SER, CLK, MSBFIRST, (data >> 8));
-    shiftOut(SER, CLK, MSBFIRST, data);
+*/   
+    shiftOut(SER, CLK, LSBFIRST, data);
+    shiftOut(SER, CLK, LSBFIRST, (data >> 8));
     digitalWrite(LATCH, HIGH);
     //delay(2);
   };
 };
 
-
-void loop() {
+void loop()
+{
   push_rnd();
-  for (int i = 0; i < random(100, 200); i++) {
+  for (int i = 0; i < 200; i++)
+  {
     draw_cycle();
   };
 }
